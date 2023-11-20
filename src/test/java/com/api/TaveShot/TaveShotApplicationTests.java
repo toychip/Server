e44f4d490848c617f5b3515b9ac64c5c333c2007
toychip <dlawnsgud427@naver.com>
2023-11-20 23:11:12 +0900

    Initial commit

---
+++ src/test/java/com/api/TaveShot/TaveShotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.TaveShot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaveShotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
