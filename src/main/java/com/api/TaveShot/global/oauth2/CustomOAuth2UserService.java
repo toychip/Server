99233bb86a4778719353f59d23e809a5818ca615
toychip <dlawnsgud427@naver.com>
2023-11-28 15:44:18 +0900

    refactor: Apply Single Responsibility Principle (#7)

--- src/main/java/com/api/TaveShot/global/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/api/TaveShot/global/oauth2/CustomOAuth2UserService.java
@@ -3,7 +3,6 @@ package com.api.TaveShot.global.oauth2;
 import com.api.TaveShot.domain.Member.Member;
 import com.api.TaveShot.domain.Member.MemberRepository;
 import java.util.Map;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -21,37 +20,38 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-
         OAuth2User oAuth2User = super.loadUser(userRequest);
         log.info("-------------- 갖고 온 정보 -------------- getAttributes : {} ", oAuth2User.getAuthorities());
 
-        // GitHub에서 반환된 사용자 정보에서 이메일 추출
         Map<String, Object> attributes = oAuth2User.getAttributes();
+        Member member = processOAuthUser(attributes);
+        return createCustomOauth2User(member, oAuth2User);
+    }
+
+    private Member processOAuthUser(Map<String, Object> attributes) {
         String gitLoginId = (String) attributes.get("login");
         Long gitId = ((Integer) attributes.get("id")).longValue();
         String gitEmail = (String) attributes.get("email");
         String gitName = (String) attributes.get("name");
 
-        Optional<Member> findMember = memberRepository.findByGitId(gitId);
+        return memberRepository.findByGitId(gitId)
+                .orElseGet(() -> registerNewMember(gitId, gitLoginId, gitEmail, gitName));
+    }
 
-        if (findMember.isEmpty()) {
-            Member newMember = Member.builder()
-                    .gitId(gitId)
-                    .gitLoginId(gitLoginId)
-                    .gitEmail(gitEmail)
-                    .gitName(gitName)
-                    .build();
+    private Member registerNewMember(Long gitId, String gitLoginId, String gitEmail, String gitName) {
+        Member newMember = Member.builder()
+                .gitId(gitId)
+                .gitLoginId(gitLoginId)
+                .gitEmail(gitEmail)
+                .gitName(gitName)
+                .build();
 
-            memberRepository.save(newMember);
+        return memberRepository.save(newMember);
+    }
 
-            return CustomOauth2User.builder()
-                    .member(newMember)
-                    .attributes(oAuth2User.getAttributes())
-                    .build();
-        }
-
+    private CustomOauth2User createCustomOauth2User(Member member, OAuth2User oAuth2User) {
         return CustomOauth2User.builder()
-                .member(findMember.get())
+                .member(member)
                 .attributes(oAuth2User.getAttributes())
                 .build();
     }
