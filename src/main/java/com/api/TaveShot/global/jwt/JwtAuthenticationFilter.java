8e53e0521b06fd2c8f2a1c14f9a84a8b9061577e
toychip <dlawnsgud427@naver.com>
2023-11-29 16:36:50 +0900

    feat: Access Token 검증 구현 (#7)

--- src/main/java/com/api/TaveShot/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/api/TaveShot/global/jwt/JwtAuthenticationFilter.java
@@ -5,9 +5,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -23,10 +29,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         if (authorizationHeader != null) {
             // ToDo Access Token 검증
-            //  jwtProvider.isValidToken(authorizationHeader);
-
-            filterChain.doFilter(request, response);
+              jwtProvider.isValidToken(authorizationHeader);
         }
+
+        filterChain.doFilter(request, response);
 
     }
 
--- src/main/java/com/api/TaveShot/global/jwt/JwtProvider.java
+++ src/main/java/com/api/TaveShot/global/jwt/JwtProvider.java
@@ -3,6 +3,7 @@ package com.api.TaveShot.global.jwt;
 import static com.api.TaveShot.global.constant.OauthConstant.ACCESS_TOKEN_VALID_TIME;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -32,10 +33,12 @@ public class JwtProvider {
                 .compact();
     }
 
+    // JWT claims 생성
     private Claims createClaims(String id) {
         return Jwts.claims().setSubject(id);
     }
 
+    // JWT 만료 시간 계산
     private long calculateExpirationDate(Date now) {
         return now.getTime() + ACCESS_TOKEN_VALID_TIME;
     }
@@ -44,4 +47,19 @@ public class JwtProvider {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
     }
 
+    // 토큰의 유효성 검사
+    public void isValidToken(String token) {
+        try {
+            SecretKey key = generateKey();
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+
+        } catch (ExpiredJwtException e) { // 어세스 토큰 만료
+            throw new IllegalArgumentException("Access Token expired");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("User Not Authorized");
+        }
+    }
 }
