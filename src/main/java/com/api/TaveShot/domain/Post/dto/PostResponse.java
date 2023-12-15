60c121ae02b3d1c8911dde89ef0cd48a19d73d81
hyeonbin <basilla0617@naver.com>
2023-12-15 20:00:24 +0900

    refactor: 변경된 파일 이름 업데이트 (CommentDto.Response->CommentResponse) (#5)

--- src/main/java/com/api/TaveShot/domain/Post/dto/PostResponse.java
+++ src/main/java/com/api/TaveShot/domain/Post/dto/PostResponse.java
@@ -1,7 +1,6 @@
 package com.api.TaveShot.domain.Post.dto;
 
-import com.api.TaveShot.domain.Comment.dto.CommentDto;
-import com.api.TaveShot.domain.Comment.dto.CommentDto.Response;
+import com.api.TaveShot.domain.Comment.dto.CommentResponse;
 import com.querydsl.core.annotations.QueryProjection;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -19,7 +18,7 @@ public class PostResponse {
     private String writer;
     private int view;
     private Long writerId;
-    private List<CommentDto.Response> comments;
+    private List<CommentResponse> comments;
 
     @QueryProjection
     public PostResponse(Long postId, String title, String content, String writer, int view, Long writerId) {
