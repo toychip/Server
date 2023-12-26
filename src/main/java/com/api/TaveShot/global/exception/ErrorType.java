1fde2b095e402dbbf9b799230abc146f2d267717
toychip <dlawnsgud427@naver.com>
2023-12-26 19:00:21 +0900

    fix: ErrorType merge 누락 정보 추가 (#13)

--- src/main/java/com/api/TaveShot/global/exception/ErrorType.java
+++ src/main/java/com/api/TaveShot/global/exception/ErrorType.java
@@ -27,11 +27,23 @@ public enum ErrorType {
     _SERVER_USER_NOT_FOUND(INTERNAL_SERVER_ERROR, "SERVER_5001", "로그인이 성공한 소셜 로그인 유저가 DB에 존재하지 않을 수 없습니다."),
 
 
+
     // ---------------------------------------- JWT TOKEN ----------------------------------------
     _JWT_PARSING_ERROR(BAD_REQUEST, "JWT_4001", "JWT 토큰 파싱 중 오류가 발생했습니다."),
     _JWT_EXPIRED(UNAUTHORIZED, "JWT_4010", "Jwt Token의 유효 기간이 만료되었습니다."),
 
 
+    // ------------------------------------------ POST ------------------------------------------
+    _POST_INVALID_TIER(BAD_REQUEST, "POST_4000", "게시글의 등급이 잘못되었습니다."),
+    _POST_NOT_FOUND(NOT_FOUND, "POST_4040", "해당 게시글이 존재하지 않습니다."),
+    _POST_USER_FORBIDDEN(FORBIDDEN, "POST_4030", "해당 게시글에 접근 권한이 없습니다."),
+
+
+    // ------------------------------------------ S3 ------------------------------------------
+    EXCEEDING_FILE_COUNT(BAD_REQUEST, "S4001", "사진 개수가 너무 많습니다."),
+    S3_UPLOAD(INTERNAL_SERVER_ERROR, "S5001", "서버오류, S3 사진 업로드 에러입니다."),
+    S3_CONNECT(INTERNAL_SERVER_ERROR, "S5002", "서버오류, S3 연결 에러입니다."),
+    S3_CONVERT(INTERNAL_SERVER_ERROR, "S5003", "서버오류, S3 변환 에러입니다."),
 
     // ------------------------------------------ USER ------------------------------------------
     _USER_NOT_FOUND_BY_TOKEN(NOT_FOUND, "USER_4040", "제공된 토큰으로 사용자를 찾을 수 없습니다."),
