125bd647300f8b2f6f1f12503a415a6be484cd8c
toychip <dlawnsgud427@naver.com>
2023-12-28 16:17:05 +0900

    fix: 잘못된 GitHubOAuth Login 경로 수정 (#22)

--- src/main/java/com/api/TaveShot/global/security/oauth2/GithubOAuthController.java
+++ src/main/java/com/api/TaveShot/global/security/oauth2/GithubOAuthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/oauth/authorize")
+@RequestMapping("login/oauth/authorize")
 public class GithubOAuthController {
 
     @GetMapping("/github")
