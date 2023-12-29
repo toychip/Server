74d6c1a60b907c221a096d16debcc14bf8b53f30
toychip <dlawnsgud427@naver.com>
2023-12-29 23:51:12 +0900

    refactor: Comment 변수명 변경 Comment content로 변경 (#5)
    Comment의 Comment filed Content로 변경

--- src/main/java/com/api/TaveShot/domain/comment/converter/CommentConverter.java
+++ src/main/java/com/api/TaveShot/domain/comment/converter/CommentConverter.java
@@ -11,34 +11,34 @@ public class CommentConverter {
 
     public static Comment createDtoToEntity(final String comment, final Member member, final Post post) {
         return Comment.builder()
-                .comment(comment)
+                .content(comment)
                 .member(member)
                 .post(post)
-                .parentComment(null) // 대댓글이 아닌 경우 parentComment는 null
+                .parent(null) // 대댓글이 아닌 경우 parentComment는 null
                 .build();
     }
 
     public static Comment createDtoToEntity(final String comment, final Member member,
                                             final Post post, final Comment parentComment) {
         return Comment.builder()
-                .comment(comment)
+                .content(comment)
                 .member(member)
                 .post(post)
-                .parentComment(parentComment)
+                .parent(parentComment)
                 .build();
     }
-
-    public static CommentResponse entityToDto(final Comment commentEntity) {
-        List<CommentResponse> replies = commentEntity.getChildComments().stream()
-                .map(CommentConverter::entityToDto)
-                .toList();
-
-        return new CommentResponse(
-                commentEntity.getId(),
-                commentEntity.getComment(),
-                commentEntity.getMember().getGitLoginId(),
-                commentEntity.getPost().getId(),
-                commentEntity.getParentComment() != null ? entityToDto(commentEntity.getParentComment()) : null,
-                replies);
-    }
+//
+//    public static CommentResponse entityToDto(final Comment commentEntity) {
+//        List<CommentResponse> replies = commentEntity.getChildComments().stream()
+//                .map(CommentConverter::entityToDto)
+//                .toList();
+//
+//        return new CommentResponse(
+//                commentEntity.getId(),
+//                commentEntity.getComment(),
+//                commentEntity.getMember().getGitLoginId(),
+//                commentEntity.getPost().getId(),
+//                commentEntity.getParentComment() != null ? entityToDto(commentEntity.getParentComment()) : null,
+//                replies);
+//    }
 }
