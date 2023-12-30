f0bd1f649e04f53ed014ff588ae236ab5befeaec
toychip <dlawnsgud427@naver.com>
2023-12-30 22:46:19 +0900

    fix: 순환 참조와 무한반복이 일어나지 않게 field 변경 (#5)
    List<Comment>가 아닌, parentId를 둠으로써 순환참조 버그 해결

--- src/main/java/com/api/TaveShot/domain/comment/dto/response/CommentResponse.java
+++ src/main/java/com/api/TaveShot/domain/comment/dto/response/CommentResponse.java
@@ -1,36 +1,8 @@
 package com.api.TaveShot.domain.comment.dto.response;
 
-import com.api.TaveShot.domain.comment.converter.CommentConverter;
-import com.api.TaveShot.domain.comment.domain.Comment;
-import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
-import lombok.Getter;
-
-import java.util.List;
 
 
-@Getter
 @Builder
-public class CommentResponse {
-    private final Long id;
-    private final String content;
-    private final String memberId;
-    private final Long postId;
-    private final CommentResponse parent;
-    private final List<CommentResponse> replies;
-
-    @QueryProjection
-    @Builder
-    public CommentResponse(Long id, String content, String memberId, Long postId, Comment parent, List<CommentResponse> replies) {
-        this.id = id;
-        this.content = content;
-        this.memberId = memberId;
-        this.postId = postId;
-        this.parent = CommentConverter.getParentResponse(parent);
-        this.replies = replies;
-    }
-
-    public static CommentResponse fromEntity(Comment commentEntity) {
-        return CommentConverter.entityToDto(commentEntity);
-    }
+public record CommentResponse(Long id, String content, String gitLoginId, Long postId, Long parentId) {
 }
