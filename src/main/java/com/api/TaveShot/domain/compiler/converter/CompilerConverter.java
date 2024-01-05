df05c1e2fcb6ba1771b3100b62081e020df33ced
hyeonbin <basilla0617@naver.com>
2024-01-05 13:26:41 +0900

    feat: CompilerConverter 구현 (#28)

---
+++ src/main/java/com/api/TaveShot/domain/compiler/converter/CompilerConverter.java
@@ -0,0 +1,16 @@
+package com.api.TaveShot.domain.compiler.converter;
+
+import com.api.TaveShot.domain.compiler.dto.ProblemInfoDto;
+import com.api.TaveShot.domain.compiler.dto.ProblemResponseDto;
+
+public class CompilerConverter {
+
+    public static ProblemInfoDto responseToInfoDto(final ProblemResponseDto response){
+        return new ProblemInfoDto(
+                response.getProblemId(),
+                response.getTitleKo(),
+                response.getLevel()
+        );
+    }
+
+}
