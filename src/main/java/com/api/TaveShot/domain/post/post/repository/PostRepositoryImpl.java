b76ae9c60e2a1103c934c0c72ebc9dfb316b60cd
toychip <dlawnsgud427@naver.com>
2024-01-08 01:31:40 +0900

    feat: 페이징 조회시 사이즈 검증 추가 (#32)

--- src/main/java/com/api/TaveShot/domain/post/post/repository/PostRepositoryImpl.java
+++ src/main/java/com/api/TaveShot/domain/post/post/repository/PostRepositoryImpl.java
@@ -3,11 +3,15 @@ package com.api.TaveShot.domain.post.post.repository;
 
 import static com.api.TaveShot.domain.post.image.domain.QImage.*;
 import static com.api.TaveShot.domain.post.post.domain.QPost.post;
+import static com.api.TaveShot.global.constant.OauthConstant.MAX_PAGE_NUMBER;
+import static com.api.TaveShot.global.constant.OauthConstant.MAX_PAGE_SIZE;
 
 import com.api.TaveShot.domain.post.post.domain.Post;
 import com.api.TaveShot.domain.post.post.domain.PostTier;
 import com.api.TaveShot.domain.post.post.dto.request.PostSearchCondition;
 import com.api.TaveShot.domain.post.post.dto.response.PostResponse;
+import com.api.TaveShot.global.exception.ApiException;
+import com.api.TaveShot.global.exception.ErrorType;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -34,6 +38,9 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
     }
 
     private List<PostResponse> getSearchPageContent(final PostSearchCondition condition, final Pageable pageable) {
+
+        validatePaging(condition, pageable);
+
         List<Post> posts = jpaQueryFactory
                 .select(post)
                 .from(post)
@@ -52,6 +59,26 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
         List<PostResponse> postResponseList = toPostResponses(posts);
 
         return postResponseList;
+    }
+
+    private void validatePaging(PostSearchCondition condition, Pageable pageable) {
+
+        if (pageable.getPageSize() > MAX_PAGE_SIZE) {
+            throw new ApiException(ErrorType._PAGING_INVALID_PAGE_SIZE);
+        }
+
+        if (pageable.getPageSize() > MAX_PAGE_NUMBER) {
+            throw new ApiException(ErrorType._PAGING_INVALID_PAGE_NUMBER);
+        }
+
+        long totalPosts = getSearchPageCount(condition).fetchOne();
+        long expectedStartIndex = pageable.getOffset();
+        long expectedEndIndex = expectedStartIndex + pageable.getPageSize();
+
+        // 데이터 범위 초과 여부 확인
+        if (expectedStartIndex >= totalPosts || expectedEndIndex > totalPosts) {
+            throw new ApiException(ErrorType._PAGING_INVALID_DATA_SIZE);
+        }
     }
 
     private BooleanExpression judgeTier(final PostTier postTierEnum) {
