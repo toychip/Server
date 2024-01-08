944cfeceb810cd88a6b357a730d1fa22ac43a259
hyeonbin <basilla0617@naver.com>
2024-01-08 16:22:16 +0900

    refactor: POST 요청 결과를 기반으로 GET 요청을 수행하도록 변경, converter 분리 (#28)

--- src/main/java/com/api/TaveShot/domain/compiler/converter/CompilerConverter.java
+++ src/main/java/com/api/TaveShot/domain/compiler/converter/CompilerConverter.java
@@ -2,6 +2,8 @@ package com.api.TaveShot.domain.compiler.converter;
 
 import com.api.TaveShot.domain.compiler.dto.ProblemInfoDto;
 import com.api.TaveShot.domain.compiler.dto.ProblemResponseDto;
+import com.api.TaveShot.domain.compiler.dto.SubmissionResponseDto;
+import com.api.TaveShot.domain.compiler.dto.SubmissionResultDto;
 
 public class CompilerConverter {
 
@@ -12,6 +14,34 @@ public class CompilerConverter {
                 response.getLevel(),
                 response.getTags()
         );
+    }
+
+    public static SubmissionResultDto responseToSubmissionResultDto(final SubmissionResponseDto details) {
+        boolean isSuccess = false;
+        String message = "Unknown";
+
+        if (details.getResult() != null && details.getResult().getStatus() != null) {
+            int statusCode = details.getResult().getStatus().getCode();
+            switch (statusCode) {
+                case 15: // 성공
+                    isSuccess = true;
+                    message = "Success";
+                    break;
+                case 11: // 컴파일 에러
+                    message = "Compilation Error";
+                    break;
+                case 12: // 런타임 에러
+                    message = "Runtime Error";
+                    break;
+                default: // 기타 오류
+                    message = "Error: Unknown or unhandled error";
+                    break;
+            }
+        } else {
+            message = "API response is missing the result data";
+        }
+
+        return new SubmissionResultDto(isSuccess, message);
     }
 
 }
--- src/main/java/com/api/TaveShot/domain/compiler/service/CompilerService.java
+++ src/main/java/com/api/TaveShot/domain/compiler/service/CompilerService.java
@@ -4,22 +4,21 @@ import com.api.TaveShot.domain.compiler.converter.CompilerConverter;
 import com.api.TaveShot.domain.compiler.dto.*;
 import com.api.TaveShot.global.exception.ApiException;
 import com.api.TaveShot.global.exception.ErrorType;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientException;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
+
+
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,15 +28,34 @@ public class CompilerService {
 
     private final WebClient solvedwebClient;
     private final WebClient sphereEngineWebClient;
-    private final String sphereEngineAccessToken = "fb3a5fa53ca96f45c72c8d0eab9c8207";
+
+    @Value("${compiler.token}")
+    private String sphereEngineAccessToken;
 
     public CompilerService(WebClient.Builder webClientBuilder) {
         this.solvedwebClient = webClientBuilder.baseUrl("https://solved.ac/api/v3").build();
         this.sphereEngineWebClient = webClientBuilder.baseUrl("https://5318779a.compilers.sphere-engine.com/api/v4")
                 .defaultHeader("Content-Type", "application/json")
+                .filter(logRequest())
+                .filter(logResponse())
                 .build();
     }
 
+    private ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
+            return Mono.just(clientRequest);
+        });
+    }
+
+    private ExchangeFilterFunction logResponse() {
+        return ExchangeFilterFunction.ofResponseProcessor(clientResponse -> {
+            log.info("Response: {} {}", clientResponse.statusCode(), clientResponse.headers().asHttpHeaders());
+            return Mono.just(clientResponse);
+        });
+    }
+
+    // 문제 정보 가져오기
     public Mono<ProblemInfoDto> getProblemInfo(@NotNull int problemId) {
         return solvedwebClient.get()
                 .uri(uriBuilder -> uriBuilder.path("/problem/show")
@@ -52,55 +70,45 @@ public class CompilerService {
 
     }
 
+    // 코드 컴파일 및 채점 결과 반환
     public Mono<SubmissionResultDto> compileAndJudge(CodeSubmissionDto submission) {
+        return submitCode(submission)
+                .flatMap(this::getSubmissionResult)
+                .delayElement(Duration.ofSeconds(10));
+    }
+
+    // Sphere Engine API에 코드 제출
+    private Mono<Integer> submitCode(CodeSubmissionDto submission) {
+        Map<String, Object> requestBody = createRequestBody(submission);
+
         return sphereEngineWebClient.post()
                 .uri(uriBuilder -> uriBuilder.path("/submissions")
                         .queryParam("access_token", sphereEngineAccessToken)
                         .build())
-                .body(BodyInserters.fromValue(createRequestBody(submission)))
+                .body(BodyInserters.fromValue(requestBody))
                 .retrieve()
                 .bodyToMono(SubmissionResponseDto.class)
-                .map(this::processSubmissionResponse);
+                .map(SubmissionResponseDto::getId);
     }
 
+    // 제출된 코드의 채점 결과 조회
+    private Mono<SubmissionResultDto> getSubmissionResult(int submissionId) {
+        return sphereEngineWebClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/submissions/" + submissionId)
+                        .queryParam("access_token", sphereEngineAccessToken)
+                        .build())
+                .retrieve()
+                .bodyToMono(SubmissionResponseDto.class)
+                .map(CompilerConverter::responseToSubmissionResultDto);
+    }
+
+    // Sphere Engine API 요청을 위한 요청 본문 생성
     private Map<String, Object> createRequestBody(CodeSubmissionDto submission) {
         Map<String, Object> body = new HashMap<>();
-        body.put("source", submission.getSourceCode());
+        body.put("source", submission.getSource());
         body.put("compilerId", submission.getCompilerId());
 
-        if (submission.getInput() != null) {
-            body.put("input", submission.getInput());
-        }
         return body;
     }
-
-    private SubmissionResultDto processSubmissionResponse(SubmissionResponseDto response) {
-
-        boolean isSuccess;
-        String message;
-
-        if (response.getResult() != null && response.getResult().getStatus() != null) {
-            int statusCode = response.getResult().getStatus().getCode();
-            if (statusCode == 15) { // 성공
-                isSuccess = true;
-                message = "Success";
-            } else if (statusCode == 11) { // 컴파일 에러
-                isSuccess = false;
-                message = "Compilation Error";
-            } else if (statusCode == 12) { // 런타임 에러
-                isSuccess = false;
-                message = "Runtime Error";
-            } else { // 기타 채점 결과 상태에 따른 처리
-                isSuccess = false;
-                message = "Error: Unknown or unhandled error";
-            }
-        } else {
-            // API가 결과를 반환하지 않았을 경우
-            isSuccess = false;
-            message = "API response is missing the result data";
-        }
-
-        return new SubmissionResultDto(isSuccess, message);
-        }
 
 }
