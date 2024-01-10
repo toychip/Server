573d35ee0952f3acfee8131b8fbdc78f0928058c
hyeonbin <basilla0617@naver.com>
2024-01-10 16:25:52 +0900

    refactor: 필드 값 JSON으로 직접 변환 (역슬래시 출력 제거) (#28)

--- src/main/java/com/api/TaveShot/domain/problem/dto/ProblemDto.java
+++ src/main/java/com/api/TaveShot/domain/problem/dto/ProblemDto.java
@@ -1,5 +1,6 @@
 package com.api.TaveShot.domain.problem.dto;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import com.opencsv.bean.CsvBindByName;
 import lombok.*;
 
@@ -9,25 +10,31 @@ import lombok.*;
 @Builder
 public class ProblemDto {
 
+    @JsonRawValue
     @CsvBindByName(column = "ID")
     private String ID;
 
+    @JsonRawValue
     @CsvBindByName(column = "Title")
     private String Title;
 
+    @JsonRawValue
     @CsvBindByName(column = "Description")
     private String Description;
 
+    @JsonRawValue
     @CsvBindByName(column = "Input Description")
     private String inputDescription;
 
+    @JsonRawValue
     @CsvBindByName(column = "Output Description")
     private String outputDescription;
 
+    @JsonRawValue
     @CsvBindByName(column = "Sample Input")
     private String sampleInput;
 
+    @JsonRawValue
     @CsvBindByName(column = "Sample Output")
     private String sampleOutput;
-
 }
