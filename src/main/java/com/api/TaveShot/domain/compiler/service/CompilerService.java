9d79066a18ab279250e5ade4c3a64c6dd0902986
hyeonbin <basilla0617@naver.com>
2024-01-21 17:58:31 +0900

    refactor: 제출 페이지가 없는 문제 번호에 접근 시(채점 준비 중인 문제) 예외 처리 적용 (#28)

--- src/main/java/com/api/TaveShot/domain/compiler/service/CompilerService.java
+++ src/main/java/com/api/TaveShot/domain/compiler/service/CompilerService.java
@@ -1,34 +1,43 @@
 package com.api.TaveShot.domain.compiler.service;
 
 import com.api.TaveShot.domain.compiler.dto.SubmissionRequestDto;
+import com.api.TaveShot.global.exception.ApiException;
+import com.api.TaveShot.global.exception.ErrorType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class CompilerService {
 
+    private static final String SUBMIT_CODE_URL = "http://localhost:5000/submitCode";
+    private static final String RESULT_URL = "http://localhost:5000/result/";
+    private static final int MAX_ATTEMPTS = 10;
+    private static final long SLEEP_TIME_MILLIS = 5000;
+
     public String submitCode(SubmissionRequestDto submissionRequestDto) {
-
         RestTemplate restTemplate = new RestTemplate();
-        String submitUrl = "http://localhost:5000/submitCode";
-        String resultUrl = "http://localhost:5000/result/";
 
-        ResponseEntity<Map> submitResponse = restTemplate.postForEntity(submitUrl, submissionRequestDto, Map.class);
+        ResponseEntity<Map> submitResponse = restTemplate.postForEntity(SUBMIT_CODE_URL, submissionRequestDto, Map.class);
         String submissionId = (String) submitResponse.getBody().get("submission_id");
 
         String result = "결과 처리 중";
-        while (result.equals("결과 처리 중")) {
-            ResponseEntity<Map> resultResponse = restTemplate.getForEntity(resultUrl + submissionId, Map.class);
+        int attempts = 0;
+        while (result.equals("결과 처리 중") && attempts < MAX_ATTEMPTS) {
+            ResponseEntity<Map> resultResponse = restTemplate.getForEntity(RESULT_URL + submissionId, Map.class);
             result = (String) resultResponse.getBody().get("result");
+            if (result.contains("채점이 준비되지 않아, 코드를 제출할 수 없습니다.")) {
+                throw new ApiException(ErrorType._SUBMIT_PAGE_NOT_FOUND);
+            }
             try {
-                Thread.sleep(2000);  // 2초 간격으로 확인
+                Thread.sleep(SLEEP_TIME_MILLIS);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
+            attempts++;
         }
+
         return result;
     }
 }
