1c1481537be4915d2b0346fbd8fcdde22e768a89
wjdrhs3473 <110841041+wjd4204@users.noreply.github.com>
2024-01-22 13:34:05 +0900

    feat: parameter를 받아 돌려진 모델의 문제에 대한 세부정보를 response에 추가 (#46)

--- src/main/java/com/api/TaveShot/domain/recommend/controller/RecommendController.java
+++ src/main/java/com/api/TaveShot/domain/recommend/controller/RecommendController.java
@@ -1,6 +1,7 @@
 package com.api.TaveShot.domain.recommend.controller;
 
 import com.api.TaveShot.domain.post.post.dto.response.PostResponse;
+import com.api.TaveShot.domain.recommend.dto.RecProRequestDto;
 import com.api.TaveShot.domain.recommend.dto.RecProResponseDto;
 import com.api.TaveShot.domain.recommend.dto.RecResponseDto;
 import com.api.TaveShot.domain.recommend.service.RecommendService;
@@ -8,6 +9,7 @@ import com.api.TaveShot.global.success.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -30,9 +33,7 @@ public class RecommendController {
     @Operation(summary = "사용자 기반 문제 추천", description = "해당 유저가 푼 문제들을 바탕으로" +
             " 같은 티어의 사용자가 푼 문제들을 관련성이 높은 순으로 추천합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "추천 성공",
-                    content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
-                            schema = @Schema(implementation = PostResponse.class)))
+            @ApiResponse(responseCode = "200", description = "추천 성공")
     })
     @GetMapping("/rival")
     public SuccessResponse<RecResponseDto> getUserBasedProList() throws IOException {
@@ -42,13 +43,11 @@ public class RecommendController {
 
     @Operation(summary = "태그 관련 문제 추천", description = "원하는 문제와 태그가 같은 관련성이 높은 문제를 추천합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "추천 성공",
-                    content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
-                            schema = @Schema(implementation = PostResponse.class)))
+            @ApiResponse(responseCode = "200", description = "추천 성공")
     })
     @GetMapping("/problem")
-    public SuccessResponse<RecResponseDto> getSolvedProList() throws IOException {
-        RecResponseDto responseDto = recommendService.getListByProblem();
+    public SuccessResponse<RecResponseDto> getSolvedProList(@RequestParam(value = "solvedRecentId") int solvedNumber) throws IOException {
+        RecResponseDto responseDto = recommendService.getListByProblem(solvedNumber);
         return new SuccessResponse<>(responseDto);
     }
 }
