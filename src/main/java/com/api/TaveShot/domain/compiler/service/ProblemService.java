b7d9a877f1b860140f091cf976d29524fb14d8df
hyeonbin <basilla0617@naver.com>
2024-01-24 21:30:05 +0900

    refactor: @RequiredArgsConstructor 추가, @Autowired 제거 (생성자 1개) (#28)

--- src/main/java/com/api/TaveShot/domain/compiler/service/ProblemService.java
+++ src/main/java/com/api/TaveShot/domain/compiler/service/ProblemService.java
@@ -6,26 +6,22 @@ import com.api.TaveShot.domain.compiler.dto.ProblemDto;
 import com.api.TaveShot.domain.compiler.repository.ProblemRepository;
 import com.api.TaveShot.global.exception.ApiException;
 import com.api.TaveShot.global.exception.ErrorType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
+@RequiredArgsConstructor
 public class ProblemService {
 
     private final ProblemRepository problemRepository;
-    private final ProblemConverter problemConverter;
 
-    @Autowired
-    public ProblemService(ProblemRepository problemRepository, ProblemConverter problemConverter) {
-        this.problemRepository = problemRepository;
-        this.problemConverter = problemConverter;
-    }
-
-    public ProblemDto getProblemById(String id) {
-        BojProblem bojProblem = problemRepository.findById(id)
+    public Optional<ProblemDto> getProblemById(String id) {
+        return problemRepository.findById(id)
+                .map(ProblemConverter::convertToDto)
                 .orElseThrow(() -> new ApiException(ErrorType._PROBLEM_NOT_FOUND));
-
-        return problemConverter.convertToDto(bojProblem);
     }
 }
