46dabbd8af70586e3b4aaab22beebc5303a37680
hyeonbin <basilla0617@naver.com>
2024-01-24 20:39:36 +0900

    refactor: @Repository 제거 (#28)

--- src/main/java/com/api/TaveShot/domain/compiler/repository/ProblemRepository.java
+++ src/main/java/com/api/TaveShot/domain/compiler/repository/ProblemRepository.java
@@ -2,8 +2,5 @@ package com.api.TaveShot.domain.compiler.repository;
 
 import com.api.TaveShot.domain.compiler.domain.BojProblem;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface ProblemRepository extends JpaRepository<BojProblem, String> {
 }
