5d033899876b2a2749a20f49632fc19a50c34749
hyeonbin <90017767+hyeonbinn@users.noreply.github.com>
2024-01-26 17:24:30 +0900

    feat 토큰 없이 접근 허용하는 경로에 uri 추가 수정 (#64)
    * feat: SecurityConfig에 compile, search 경로 수정 (#60)
    
    * feat: SecurityConfig에 compile, search 경로 수정 (#63)
    
    * feat: SecurityConfig에 compile, search 경로 수정 (#63)

--- src/main/java/com/api/TaveShot/global/config/SecurityConfig.java
+++ src/main/java/com/api/TaveShot/global/config/SecurityConfig.java
@@ -42,8 +42,8 @@ public class SecurityConfig {
                                         ,"/favicon.ico"
                                         ,"/login/**"
                                         , "/api/health"
-                                        , "/api/v1/search"
-                                        , "/api/compile"
+                                        , "/api/v1/search/**"
+                                        , "/api/compile/**"
                                 ).permitAll()
                                 .anyRequest().authenticated());
 
